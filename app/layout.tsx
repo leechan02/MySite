340483725c5abb57582a5623a6406815f65ad7f1
Eui Lee <lechan1317@gmail.com>
2024-09-10 01:59:17 +0900

    init: footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "@/styles/globals.css";
 import { ReduxProvider } from "@/lib/redux/provider";
 import { ThemeProvider } from "@/components/ThemeProvider";
+import Footer from "@/components/Footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -33,6 +34,7 @@ export default function RootLayout({
             className={`${geistSans.variable} ${geistMono.variable} antialiased`}
           >
             {children}
+            <Footer />
           </body>
         </ThemeProvider>
       </ReduxProvider>
--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,6 @@ export default function Home() {
   return (
     <div className="p-4">
       <h1 className="text-2xl font-bold mb-4">Welcome to Euiclee's website</h1>
-      <DarkModeSwitch />
-      <LangModeSwitch />
     </div>
   );
 }
