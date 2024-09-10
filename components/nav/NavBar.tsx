dfe839eee6b9c1994e6e1515fbb4ebdc3e35b240
Eui Lee <lechan1317@gmail.com>
2024-09-11 00:16:28 +0900

    style: navbar style

--- components/nav/NavBar.tsx
+++ components/nav/NavBar.tsx
@@ -1,16 +1,14 @@
 export default function NavBar() {
   return (
-    <div className='w-[156px] h-[50px] py-1 flex-col justify-start items-start inline-flex'>
-      <div className='self-stretch p-1 bg-[#353535]/5 rounded-full shadow border-2 border-[#353535]/10 backdrop-blur-[15px] justify-start items-center gap-1 inline-flex'>
-        <div className='h-[34px] py-2 rounded-full justify-center items-center flex'>
-          <div className="text-center text-[#353535] text-xs font-semibold font-['Inter'] leading-[18px] tracking-tight">
-            Work
-          </div>
+    <div className='p-1 bg-primary/5 rounded-full shadow border-2 border-primary/10 backdrop-blur-[15px] justify-start items-center gap-1 inline-flex'>
+      <div className='w-[64px] py-2 rounded-full justify-center items-center flex'>
+        <div className='text-center text-primary text-xs font-semibold font-mono'>
+          Work
         </div>
-        <div className='h-[34px] py-2 bg-[#353535]/5 rounded-full justify-center items-center flex'>
-          <div className="text-center text-[#353535] text-xs font-semibold font-['Inter'] leading-[18px] tracking-tight">
-            Info
-          </div>
+      </div>
+      <div className='w-[64px] py-2 bg-primary/5 rounded-full justify-center items-center flex'>
+        <div className='text-center text-primary text-xs font-semibold font-mono'>
+          Info
         </div>
       </div>
     </div>
