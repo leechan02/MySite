1a188af9b40f8c962aecfa877801550ec4b84190
Eui Lee <lechan1317@gmail.com>
2024-09-12 23:57:42 +0900

    feat: navbar justify

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -3,11 +3,13 @@ import NavBar from "../nav/NavBar";
 export default function Header() {
   return (
     <header className='flex justify-between items-center px-16 py-10'>
-      <div className='text-primary text-base font-bold font-mono'>
+      <div className='text-primary text-base font-bold font-mono w-1/3 flex justify-start'>
         Euichan Lee
       </div>
-      <NavBar />
-      <div className='text-primary text-base font-bold font-mono'>
+      <div className='w-1/3 flex justify-center'>
+        <NavBar />
+      </div>
+      <div className='text-primary text-base font-bold font-mono w-1/3 flex justify-end'>
         Resume
       </div>
     </header>
