aad281d49dde601e488caae2db0566e1ac186164
Eui Lee <lechan1317@gmail.com>
2024-09-12 19:18:28 +0900

    style: button backblur

--- components/button/Button.tsx
+++ components/button/Button.tsx
@@ -8,7 +8,7 @@ export default function Button({
   icon: Icon,
 }: ButtonProps) {
   return (
-    <div className="flex justify-center items-center gap-2 rounded-full bg-primary/5 py-2 pl-4 pr-6">
+    <div className="flex justify-center items-center gap-2 rounded-full bg-primary/5 border-2 border-primary/5 backdrop-blur-md py-2 pl-4 pr-6">
       {Icon && <Icon className="w-4 h-4 text-primary" />}
       <div className="font-semibold font-mono text-primary font-xs">{label}</div>
     </div>
