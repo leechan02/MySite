6f152a19f4d8466e0e3d8402e91865aaefc58e3d
Eui Lee <lechan1317@gmail.com>
2024-09-14 00:17:18 +0900

    fix: import

--- app/(normal)/info/_components/AboutLeftContent.tsx
+++ app/(normal)/info/_components/AboutLeftContent.tsx
@@ -8,16 +8,16 @@ export default function AboutLeftContent() {
           My background in Computer Science
         </div>
         <div className='text-xs font-mono text-foreground opacity-50'>
-          In 2022, I started my journey into development without knowing
-          how to code or use a terminal. As I grew more curious about the
-          world, I recognized many inefficiencies around me and
-          realized the importance of problem-solving.
+          In 2022, I started my journey into development without knowing how to
+          code or use a terminal. As I grew more curious about the world, I
+          recognized many inefficiencies around me and realized the importance
+          of problem-solving.
         </div>
         <div className='text-xs font-mono text-foreground opacity-50'>
-          This led me to 42 Seoul, where I immersed myself in computer
-          science fundamentals such as operating systems, data
-          structures, and networks. Currently, I am focusing on frontend
-          development to address real-world challenges.
+          This led me to 42 Seoul, where I immersed myself in computer science
+          fundamentals such as operating systems, data structures, and networks.
+          Currently, I am focusing on frontend development to address real-world
+          challenges.
         </div>
       </div>
       <div className='relative w-[300px] lg:w-[400px] h-[400px] border-10 border-color1 rounded-[40px] overflow-hidden'>
@@ -34,15 +34,14 @@ export default function AboutLeftContent() {
         </div>
         <div className='text-xs font-mono text-foreground opacity-50'>
           I appreciate beauty, which is why I prioritize writing clean,
-          understandable, and simple code. While optimization is
-          important, I don't believe complex code is always
-          better-performing.
+          understandable, and simple code. While optimization is important, I
+          don't believe complex code is always better-performing.
         </div>
         <div className='text-xs font-mono text-foreground opacity-50'>
           As a developer, I value communication skills, especially since
-          development is rarely a solo effort. Starting from scratch has
-          taught me how to explain concepts clearly and in a way that
-          others can easily understand.
+          development is rarely a solo effort. Starting from scratch has taught
+          me how to explain concepts clearly and in a way that others can easily
+          understand.
         </div>
       </div>
       <div className='relative w-[300px] lg:w-[400px] h-[400px] border-10 border-color1 rounded-[40px] overflow-hidden'>
