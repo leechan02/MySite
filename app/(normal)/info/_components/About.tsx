5ce3778a80c0dd4bfb2abb63704c0b96843deb4c
Eui Lee <lechan1317@gmail.com>
2024-09-13 19:20:45 +0900

    feat: about me

--- app/(normal)/info/_components/About.tsx
+++ app/(normal)/info/_components/About.tsx
@@ -1,12 +1,16 @@
 import Chip from "@/components/chip/Chip";
 import React from "react";
 import AboutLeftContent from "./AboutLeftContent";
+import AboutRightContent from "./AboutRightContent";
 
 export default function About() {
   return (
     <div className='flex flex-col justify-center items-start max-w-[900px] gap-4'>
       <Chip label='ABOUT ME' />
-      <AboutLeftContent />
+      <div className='flex justify-center items-center gap-16'>
+        <AboutLeftContent />
+        <AboutRightContent />
+      </div>
     </div>
   );
 }
