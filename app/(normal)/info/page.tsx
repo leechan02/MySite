de52e8c60bf495e20e1d22a44d92c55a49ddb3dc
Eui Lee <lechan1317@gmail.com>
2024-09-13 23:56:50 +0900

    feat: title responsive

--- app/(normal)/info/page.tsx
+++ app/(normal)/info/page.tsx
@@ -22,12 +22,12 @@ export default async function Info() {
           </Link>
         </div>
       </div>
-      <h1 className='w-[640px] lg:w-[900px] text-xl sm:text-3xl font-black font-mono text-foreground text-center px-2'>
+      <h1 className='w-[320px] sm:w-[640px] lg:w-[900px] text-xl sm:text-3xl font-black font-mono text-foreground text-center px-2'>
         I enjoy defining problems and finding solutions to make the world a bit
         more convenient.
       </h1>
       <About />
-      <div className='w-[640px] lg:w-[900px] border border-foreground opacity-10' />
+      <div className='w-[320px] md:w-[640px] lg:w-[900px] border border-foreground opacity-10' />
       {/* <Education /> */}
       {/* <div className='w-[900px] border border-foreground opacity-10' /> */}
     </div>
