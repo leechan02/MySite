c243873c253ffdbd35d2c104049dcac2fc2f69a7
Eui Lee <lechan1317@gmail.com>
2024-09-13 15:22:42 +0900

    style: gap change

--- app/(normal)/info/page.tsx
+++ app/(normal)/info/page.tsx
@@ -4,20 +4,22 @@ import { FiGithub, FiMail, FiYoutube } from "react-icons/fi";
 export default async function Info() {
   return (
     <div className='px-10'>
-      <div className='flex flex-col justify-center items-center gap-10'>
-        <h1 className='text-3xl font-bold font-mono text-foreground'>
-          Hello I'm Euichan Lee
-        </h1>
-        <div className='flex justify-center items-center gap-10'>
-          <Link href='mailto:lechan1317@gmail.com'>
-            <FiMail className='inline text-foreground' size={20} />
-          </Link>
-          <Link href='https://github.com/leechan02' target='_blank'>
-            <FiGithub className='inline text-foreground' size={20} />
-          </Link>
-          <Link href='https://www.youtube.com/@euiclee' target='_blank'>
-            <FiYoutube className='inline text-foreground' size={20} />
-          </Link>
+      <div className='flex flex-col justify-center items-center gap-16'>
+        <div className='flex flex-col justify-center items-center gap-4'>
+          <h1 className='text-3xl font-bold font-mono text-foreground'>
+            Hello I'm Euichan Lee
+          </h1>
+          <div className='flex justify-center items-center gap-10'>
+            <Link href='mailto:lechan1317@gmail.com'>
+              <FiMail className='inline text-foreground' size={20} />
+            </Link>
+            <Link href='https://github.com/leechan02' target='_blank'>
+              <FiGithub className='inline text-foreground' size={20} />
+            </Link>
+            <Link href='https://www.youtube.com/@euiclee' target='_blank'>
+              <FiYoutube className='inline text-foreground' size={20} />
+            </Link>
+          </div>
         </div>
         <h1 className='text-3xl font-black font-mono text-foreground text-center'>
           I enjoy defining problems and finding solutions <br /> to make the
