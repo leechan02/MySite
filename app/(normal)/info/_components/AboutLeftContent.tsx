cde116c0d6cb4a9ec590f2de26f6555eff5ba083
Eui Lee <lechan1317@gmail.com>
2024-09-13 19:10:30 +0900

    feat: leftcontent

--- app/(normal)/info/_components/AboutLeftContent.tsx
+++ app/(normal)/info/_components/AboutLeftContent.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function AboutLeftContent() {
   return (
     <div>
-      <div className='flex justify-center items-center gap-4'>
-        <div className='flex flex-col justify-center items-start gap-4 w-max-[320px]'>
+      <div className='flex flex-col justify-center items-center gap-16'>
+        <div className='flex flex-col justify-center items-start gap-4'>
           <div className='text-sm font-mono text-foreground font-bold'>
             My background in Computer Science
           </div>
@@ -20,17 +20,41 @@ export default function AboutLeftContent() {
             structures, and networks. Currently, <br /> I am focusing on
             frontend development <br /> to address real-world challenges.
           </div>
-          <div className='relative w-[400px] h-[400px] border-10 border-color1 rounded-[40px] overflow-hidden'>
-            <Image
-              src='/image/profile.jpg'
-              alt='profile'
-              layout='fill'
-              objectFit='cover'
-            />
+        </div>
+        <div className='relative w-[400px] h-[400px] border-10 border-color1 rounded-[40px] overflow-hidden'>
+          <Image
+            src='/image/profile.jpg'
+            alt='profile'
+            layout='fill'
+            objectFit='cover'
+          />
+        </div>
+        <div className='flex flex-col justify-center items-start gap-4'>
+          <div className='text-sm font-mono text-foreground font-bold'>
+            Coding style
           </div>
+          <div className='text-xs font-mono text-foreground opacity-50'>
+            I appreciate beauty, which is why <br /> I prioritize writing clean,
+            understandable, and simple code. <br /> While optimization is
+            important, <br /> I don't believe complex code is always
+            better-performing.
+          </div>
+          <div className='text-xs font-mono text-foreground opacity-50'>
+            As a developer, I value communication skills, <br /> especially
+            since development is rarely a solo effort. <br /> Starting from
+            scratch has taught me how to explain <br /> concepts clearly and in
+            a way that others can easily understand.
+          </div>
+        </div>
+        <div className='relative w-[400px] h-[400px] border-10 border-color1 rounded-[40px] overflow-hidden'>
+          <Image
+            src='/image/profile.jpg'
+            alt='profile'
+            layout='fill'
+            objectFit='cover'
+          />
         </div>
       </div>
     </div>
   );
 }
-
