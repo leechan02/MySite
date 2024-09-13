dd0dd1d21620906f7667662a9f7092351ff94c24
Eui Lee <lechan1317@gmail.com>
2024-09-13 23:29:48 +0900

    style: remove br

--- app/(normal)/info/_components/AboutLeftContent.tsx
+++ app/(normal)/info/_components/AboutLeftContent.tsx
@@ -8,16 +8,16 @@ export default function AboutLeftContent() {
           My background in Computer Science
         </div>
         <div className='text-xs font-mono text-foreground opacity-50'>
-          In 2022, I started my journey into development <br /> without knowing
-          how to code or use a terminal. <br /> As I grew more curious about the
-          world, <br /> I recognized many inefficiencies around me <br /> and
+          In 2022, I started my journey into development without knowing
+          how to code or use a terminal. As I grew more curious about the
+          world, I recognized many inefficiencies around me and
           realized the importance of problem-solving.
         </div>
         <div className='text-xs font-mono text-foreground opacity-50'>
-          This led me to 42 Seoul, where I immersed myself <br /> in computer
-          science fundamentals such as operating systems, <br /> data
-          structures, and networks. Currently, <br /> I am focusing on frontend
-          development <br /> to address real-world challenges.
+          This led me to 42 Seoul, where I immersed myself in computer
+          science fundamentals such as operating systems, data
+          structures, and networks. Currently, I am focusing on frontend
+          development to address real-world challenges.
         </div>
       </div>
       <div className='relative w-[400px] h-[400px] border-10 border-color1 rounded-[40px] overflow-hidden'>
@@ -33,15 +33,15 @@ export default function AboutLeftContent() {
           Coding style
         </div>
         <div className='text-xs font-mono text-foreground opacity-50'>
-          I appreciate beauty, which is why <br /> I prioritize writing clean,
-          understandable, and simple code. <br /> While optimization is
-          important, <br /> I don't believe complex code is always
+          I appreciate beauty, which is why I prioritize writing clean,
+          understandable, and simple code. While optimization is
+          important, I don't believe complex code is always
           better-performing.
         </div>
         <div className='text-xs font-mono text-foreground opacity-50'>
-          As a developer, I value communication skills, <br /> especially since
-          development is rarely a solo effort. <br /> Starting from scratch has
-          taught me how to explain <br /> concepts clearly and in a way that
+          As a developer, I value communication skills, especially since
+          development is rarely a solo effort. Starting from scratch has
+          taught me how to explain concepts clearly and in a way that
           others can easily understand.
         </div>
       </div>
--- app/(normal)/info/_components/AboutRightContent.tsx
+++ app/(normal)/info/_components/AboutRightContent.tsx
@@ -16,20 +16,17 @@ export default function AboutRightContent() {
           But I want more
         </div>
         <div className='text-xs font-mono text-foreground opacity-50'>
-          I have a lot of curiosity and a strong desire <br /> to learn. The
-          more I study, the more I realize
-          <br /> how much I don’t know.
-          <br /> Since I've had less time to study than others,
-          <br /> I’ve worked even harder to catch up.
+          I have a lot of curiosity and a strong desire to learn. The more I
+          study, the more I realize how much I don’t know. Since I've had less
+          time to study than others, I’ve worked even harder to catch up.
         </div>
         <div className='text-xs font-mono text-foreground opacity-50'>
-          Through this, I've learned that while things <br /> can be difficult,
-          nothing is impossible. I've solved problems, <br /> completed over 300
-          code reviews, and written over
-          <br /> 10,000 lines of code in a single project. <br /> This has made
-          me unafraid of learning new technologies. <br /> Despite having no
-          prior experience with frameworks <br /> like React or Next.js,
-          <br />I built a website using Next.js in just two weeks.
+          Through this, I've learned that while things can be difficult, nothing
+          is impossible. I've solved problems, completed over 300 code reviews,
+          and written over 10,000 lines of code in a single project. This has
+          made me unafraid of learning new technologies. Despite having no prior
+          experience with frameworks like React or Next.js, I built a website
+          using Next.js in just two weeks.
         </div>
       </div>
       <div className='relative w-[400px] h-[400px] border-10 border-color1 rounded-[40px] overflow-hidden'>
@@ -45,19 +42,15 @@ export default function AboutRightContent() {
           In my spare time,
         </div>
         <div className='text-xs font-mono text-foreground opacity-50'>
-          I enjoy learning, taking on challenges,
-          <br /> and documenting my journey.
+          I enjoy learning, taking on challenges, and documenting my journey.
         </div>
         <div className='text-xs font-mono text-foreground opacity-50'>
-          Out of curiosity, I read books to learn. <br /> I love challenges, so
-          I dive in and try things—recently, <br /> I climbed the Himalayas. I
-          believe that knowledge becomes truly mine
-          <br /> when I write and speak about it, which is
-          <br />
-          why I run a YouTube channel like a personal journal. <br /> While I’m
-          not a huge fan of exercise,
-          <br /> I do it because I need to stay healthy
-          <br /> to keep doing what I love.
+          Out of curiosity, I read books to learn. I love challenges, so I dive
+          in and try things—recently, I climbed the Himalayas. I believe that
+          knowledge becomes truly mine when I write and speak about it, which is
+          why I run a YouTube channel like a personal journal. While I’m not a
+          huge fan of exercise, I do it because I need to stay healthy to keep
+          doing what I love.
         </div>
       </div>
     </div>
