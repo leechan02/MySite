5875e41ecb7d355866103264e797374aaae989ce
Eui Lee <lechan1317@gmail.com>
2024-09-14 21:51:41 +0900

    style: educatioon responsive

--- app/(normal)/info/_components/Education.tsx
+++ app/(normal)/info/_components/Education.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default function Education() {
   return (
-    <div className='flex flex-col justify-center items-start w-full max-w-[900px] gap-16'>
+    <div className='flex flex-col justify-center items-start sm:w-[640px] lg:w-[900px] gap-4'>
       <Chip label='EDUCATION' />
       <div className='flex justify-between item-start font-mono text-foreground w-full'>
         <div className='text-3xl font-bold'>42 Seoul</div>
