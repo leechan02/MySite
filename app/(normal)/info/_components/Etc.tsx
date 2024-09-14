7c3bef7bf25fedaa768732798283e49e821c1d15
Eui Lee <lechan1317@gmail.com>
2024-09-14 22:08:01 +0900

    feat: Sklills and Etc

--- app/(normal)/info/_components/Etc.tsx
+++ app/(normal)/info/_components/Etc.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import Chip from "@/components/chip/Chip";
+import React from "react";
 
 export default function Etc() {
   return (
-    <div>
-      
+    <div className='flex flex-col justify-center items-start sm:w-[640px] lg:w-[900px] gap-4'>
+      <Chip label='ETC' />
+      <div className='flex justify-start items-center gap-2 font-mono text-foreground'>
+        <div className='font-sm font-bold'>OPIc AL</div>
+        <div className='font-xs font-bold opacity-50'>2024.6.7</div>
+      </div>
+      <div className='flex justify-start items-center gap-2 font-mono text-foreground'>
+        <div className='font-sm font-bold'>
+          Craftsman Information Processing
+        </div>
+        <div className='font-xs font-bold opacity-50'>2024.7.3</div>
+      </div>
     </div>
   );
 }
-
--- app/(normal)/info/page.tsx
+++ app/(normal)/info/page.tsx
@@ -1,6 +1,9 @@
 import Link from "next/link";
 import { FiGithub, FiMail, FiYoutube } from "react-icons/fi";
 import About from "./_components/About";
+import Education from "./_components/Education";
+import Skills from "./_components/Skills";
+import Etc from "./_components/Etc";
 
 export default async function Info() {
   return (
@@ -27,6 +30,11 @@ export default async function Info() {
       </h1>
       <About />
       <div className='w-[320px] sm:w-[640px] lg:w-[900px] border border-foreground opacity-10' />
+      <Education />
+      <div className='w-[320px] sm:w-[640px] lg:w-[900px] border border-foreground opacity-10' />
+      <Skills />
+      <div className='w-[320px] sm:w-[640px] lg:w-[900px] border border-foreground opacity-10' />
+      <Etc />
     </div>
   );
 }
