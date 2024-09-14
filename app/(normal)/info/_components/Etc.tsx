0e43fdbfa678200a596320b66cd43b0bfb468be4
Eui Lee <lechan1317@gmail.com>
2024-09-14 22:15:06 +0900

    feat: info add

--- app/(normal)/info/_components/Etc.tsx
+++ app/(normal)/info/_components/Etc.tsx
@@ -5,15 +5,15 @@ export default function Etc() {
   return (
     <div className='flex flex-col justify-center items-start sm:w-[640px] lg:w-[900px] gap-4'>
       <Chip label='ETC' />
-      <div className='flex justify-start items-center gap-2 font-mono text-foreground'>
-        <div className='font-sm font-bold'>OPIc AL</div>
-        <div className='font-xs font-bold opacity-50'>2024.6.7</div>
+      <div className='flex justify-center items-center gap-2 font-mono text-foreground'>
+        <div className='text-sm font-bold'>OPIc AL</div>
+        <div className='text-xs font-bold opacity-50'>2024.6.7</div>
       </div>
-      <div className='flex justify-start items-center gap-2 font-mono text-foreground'>
-        <div className='font-sm font-bold'>
+      <div className='flex justify-center items-center gap-2 font-mono text-foreground'>
+        <div className='text-sm font-bold'>
           Craftsman Information Processing
         </div>
-        <div className='font-xs font-bold opacity-50'>2024.7.3</div>
+        <div className='text-xs font-bold opacity-50'>2024.7.3</div>
       </div>
     </div>
   );
--- app/(normal)/info/_components/Skills.tsx
+++ app/(normal)/info/_components/Skills.tsx
@@ -4,21 +4,21 @@ export default function Skills() {
   return (
     <div className='flex flex-col justify-center items-start sm:w-[640px] lg:w-[900px] gap-4'>
       <Chip label='SKILLS' />
-      <div className='flex flex-col justify-start items-center gap-2 font-mono text-foreground'>
-        <div className='font-sm font-bold'>Language</div>
-        <div className='font-xs font-bold opacity-50'>
+      <div className='flex flex-col justify-start items-start gap-2 font-mono text-foreground'>
+        <div className='text-sm font-bold'>Language</div>
+        <div className='text-xs font-bold opacity-50'>
           C/C++, JavaScript, TypeScript
         </div>
       </div>
-      <div className='flex flex-col justify-start items-center gap-2 font-mono text-foreground'>
-        <div className='font-sm font-bold'>Frontend</div>
-        <div className='font-xs font-bold opacity-50'>
+      <div className='flex flex-col justify-start items-start gap-2 font-mono text-foreground'>
+        <div className='text-sm font-bold'>Frontend</div>
+        <div className='text-xs font-bold opacity-50'>
           React, Next.js, React Query, Redux, TailwindCSS, Framer Motion
         </div>
       </div>
-      <div className='flex flex-col justify-start items-center gap-2 font-mono text-foreground'>
-        <div className='font-sm font-bold'>Tools</div>
-        <div className='font-xs font-bold opacity-50'>
+      <div className='flex flex-col justify-start items-start gap-2 font-mono text-foreground'>
+        <div className='text-sm font-bold'>Tools</div>
+        <div className='text-xs font-bold opacity-50'>
           Git/GitHub, Figma, Slack, Notion, Visual Studio Code
         </div>
       </div>
