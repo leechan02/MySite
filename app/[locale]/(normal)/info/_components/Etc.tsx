ca37089cae49d4fd2df4fc5188dbd0bc0a9082fe
Eui Lee <lechan1317@gmail.com>
2024-09-15 23:11:31 +0900

    style: etc responsive add

--- app/[locale]/(normal)/info/_components/Etc.tsx
+++ app/[locale]/(normal)/info/_components/Etc.tsx
@@ -3,17 +3,19 @@ import React from "react";
 
 export default function Etc() {
   return (
-    <div className='flex flex-col justify-center items-start sm:w-[640px] lg:w-[900px] gap-8'>
+    <div className='flex flex-col justify-center items-start w-[300px] sm:w-[640px] lg:w-[900px] gap-8 sm:gap-16'>
       <Chip label='ETC' />
-      <div className='flex justify-center items-center gap-2 font-mono text-foreground'>
-        <div className='text-sm font-bold'>OPIc AL</div>
-        <div className='text-xs font-bold opacity-50'>2024.6.7</div>
-      </div>
-      <div className='flex justify-center items-center gap-2 font-mono text-foreground'>
-        <div className='text-sm font-bold'>
-          Craftsman Information Processing
+      <div className='w-full flex flex-col justify-between items-start gap-4 sm:gap-8'>
+        <div className='flex flex-col sm:flex-row justify-start items-start sm:items-center gap-2 sm:gap-4 font-mono text-foreground'>
+          <div className='text-sm font-bold'>OPIc AL</div>
+          <div className='text-xs font-bold opacity-50'>2024.6.7</div>
         </div>
-        <div className='text-xs font-bold opacity-50'>2024.7.3</div>
+        <div className='flex flex-col sm:flex-row justify-start items-start sm:items-center gap-2 sm:gap-4 font-mono text-foreground'>
+          <div className='text-sm font-bold'>
+            Craftsman Information Processing
+          </div>
+          <div className='text-xs font-bold opacity-50'>2024.7.3</div>
+        </div>
       </div>
     </div>
   );
