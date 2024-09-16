8be65d6962b6c5a19e723d6e3862db7afcd00ca4
Eui Lee <lechan1317@gmail.com>
2024-09-16 20:03:49 +0900

    feat: transcendence page

--- app/[locale]/(projects)/transcendence/page.tsx
+++ app/[locale]/(projects)/transcendence/page.tsx
@@ -1,7 +1,31 @@
-export default async function Transcendence() {
+import Overview from "../_components/Overview";
+import Highlights from "../_components/Highlights";
+import Content from "../_components/Content";
+import TableOfContents from "../_components/TableOfContents";
+import { unstable_setRequestLocale } from "next-intl/server";
+
+export default async function Transcendence({
+  params: { locale },
+}: {
+  params: { locale: string };
+}) {
+  unstable_setRequestLocale(locale);
+
   return (
-    <div className='flex flex-col justify-center items-center'>
-      <h1 className="text-2xl font-bold mb-4">ft_transcendence Project</h1>
+    <div className='flex flex-col justify-center items-center gap-24 pb-24 font-mono text-foreground w-[320px] sm:w-[640px] lg:w-[900px] '>
+      <Content
+        link='https://github.com/42Nuts/Transcendence'
+        image='/image/transcendence.png'
+        project='transcendence.content'
+        brColor='border-color1'
+      />
+      <section id='overview'>
+        <Overview project='transcendence.overview' />
+      </section>
+      <section id='highlights'>
+        <Highlights project='transcendence.highlights' bgColor='bg-color1' />
+      </section>
+      <TableOfContents sections={["Overview", "Highlights"]} />
     </div>
   );
 }
