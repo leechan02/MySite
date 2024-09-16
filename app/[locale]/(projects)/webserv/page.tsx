efc5b4896176d41fc76f9be4343df86d889a61c5
Eui Lee <lechan1317@gmail.com>
2024-09-16 13:51:10 +0900

    style: border add

--- app/[locale]/(projects)/webserv/page.tsx
+++ app/[locale]/(projects)/webserv/page.tsx
@@ -8,7 +8,9 @@ export default async function Webserv() {
         <h1 className="text-5xl font-bold">Webserv</h1>
         <div className="font-bold opacity-50">42 Seoul July 2023</div>
       </div>
-      <Image src="/image/webserv.png" width={800} height={400} alt="webserv" />
+      <div className="flex justify-center items-center border-8 border-color2 border-opacity-50 rounded-3xl py-10 w-full">
+        <Image src="/image/webserv.png" width={600} height={400} alt="webserv" />
+      </div>
       <Overview />
       <div className='w-[320px] sm:w-[640px] lg:w-[900px] border border-foreground opacity-10' />
     </div>
