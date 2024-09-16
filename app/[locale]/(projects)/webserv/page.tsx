75a686e028bfd62884fbcba8164afe5c310b29f2
Eui Lee <lechan1317@gmail.com>
2024-09-16 12:51:13 +0900

    feat: webserv title

--- app/[locale]/(projects)/webserv/page.tsx
+++ app/[locale]/(projects)/webserv/page.tsx
@@ -1,7 +1,14 @@
+import Image from "next/image";
+
 export default async function Webserv() {
   return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4">Webserv Project</h1>
+    <div className='flex flex-col justify-center items-center gap-10 font-mono text-foreground'>
+      <div className="flex flex-col text-center gap-4">
+        <h1 className="text-5xl font-bold">Webserv</h1>
+        <div className="font-bold opacity-50">42 Seoul July 2023</div>
+      </div>
+      <Image src="/image/webserv.png" width={800} height={400} alt="webserv" />
+      <div className='w-[320px] sm:w-[640px] lg:w-[900px] border border-foreground opacity-10' />
     </div>
   );
 }
