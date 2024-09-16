6f571ab2199ce4ff36e0e2d95285a346b6d8fa5c
Eui Lee <lechan1317@gmail.com>
2024-09-17 00:32:17 +0900

    feat: education translate

--- app/[locale]/(normal)/info/_components/Education.tsx
+++ app/[locale]/(normal)/info/_components/Education.tsx
@@ -1,23 +1,26 @@
 import Chip from "@/components/chip/Chip";
 import React from "react";
 import EduContent from "./EduContent";
+import { useTranslations } from "next-intl";
 
 export default function Education() {
+  const t = useTranslations("info.education");
+
   return (
     <div className='flex flex-col justify-center items-start w-[300px] sm:w-[640px] lg:w-[900px] gap-8 sm:gap-16'>
       <Chip label='EDUCATION' />
       <div className='flex flex-col gap-8 sm:gap-16 w-full'>
         <EduContent
-          title='42 Seoul'
-          location='Seoul, Korea'
-          date='July 2022 - July 2024'
-          description='Experience in various toy projects'
+          title={t("first")}
+          location={t("firstLocation")}
+          date={t("firstDate")}
+          description={t("firstContent")}
         />
         <EduContent
-          title='Korea National Open University'
-          location='Seoul, Korea'
-          date='Sept 2023 - Present'
-          description='Experience in various toy projects'
+          title={t("second")}
+          location={t("secondLocation")}
+          date={t("secondDate")}
+          description={t("secondContent")}
         />
       </div>
     </div>
