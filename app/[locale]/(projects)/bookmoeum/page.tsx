f02f094f18a4894e76dd41917343760d93b39fec
Eui Lee <lechan1317@gmail.com>
2024-09-16 20:19:16 +0900

    feat: bookmoeum page

--- app/[locale]/(projects)/bookmoeum/page.tsx
+++ app/[locale]/(projects)/bookmoeum/page.tsx
@@ -1,7 +1,31 @@
-export default async function Bookmoeum() {
+import Overview from "../_components/Overview";
+import Highlights from "../_components/Highlights";
+import Content from "../_components/Content";
+import TableOfContents from "../_components/TableOfContents";
+import { unstable_setRequestLocale } from "next-intl/server";
+
+export default async function Bookmoeum({
+  params: { locale },
+}: {
+  params: { locale: string };
+}) {
+  unstable_setRequestLocale(locale);
+
   return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4">Bookmoeum Project</h1>
+    <div className='flex flex-col justify-center items-center gap-24 pb-24 font-mono text-foreground w-[320px] sm:w-[640px] lg:w-[900px] '>
+      <Content
+        link='https://github.com/leechan02/Bookmoeum'
+        image='/image/bookmoeum.png'
+        project='bookmoeum.content'
+        brColor='border-color4'
+      />
+      <section id='overview'>
+        <Overview project='bookmoeum.overview' />
+      </section>
+      <section id='highlights'>
+        <Highlights project='bookmoeum.highlights' bgColor='bg-color4' />
+      </section>
+      <TableOfContents sections={["Overview", "Highlights"]} />
     </div>
   );
 }
