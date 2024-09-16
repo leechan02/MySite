f5a08fbda68c75aa74868ece694026664919babc
Eui Lee <lechan1317@gmail.com>
2024-09-16 12:16:06 +0900

    style: skillpopup responsive

--- components/popup/SkillPopup.tsx
+++ components/popup/SkillPopup.tsx
@@ -22,14 +22,14 @@ export default function SkillPopup({
       onClick={onClose}
     >
       <motion.div
-        className='bg-foreground/5 shadow border-2 border-foreground/10 rounded-[40px] backdrop-blur-[15px] p-6 px-8 w-full max-w-md font-mono'
+        className='bg-foreground/5 shadow border-2 border-foreground/10 rounded-[40px] backdrop-blur-[15px] p-6 px-8 w-full max-w-[80%] sm:max-w-md font-mono'
         onClick={(e) => e.stopPropagation()}
       >
         <div className='flex flex-col items-start gap-4'>
           <MacButtons onClose={onClose} />
           <h2 className='text-xl font-bold'>{title}</h2>
           {details.map((detail, index) => (
-            <div key={index} className="flex flex-col gap-1">
+            <div key={index} className='flex flex-col gap-1'>
               <h3 className='text-sm font-bold'>{detail.skill}</h3>
               <p className='text-xs opacity-50'>{detail.description}</p>
             </div>
