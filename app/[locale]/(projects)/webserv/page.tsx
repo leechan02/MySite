fcb1e55083ef9b35c92bc20a7533126f33a996f3
Eui Lee <lechan1317@gmail.com>
2024-09-16 14:53:19 +0900

    style: gradient percent

--- app/[locale]/(projects)/webserv/page.tsx
+++ app/[locale]/(projects)/webserv/page.tsx
@@ -9,7 +9,7 @@ export default async function Webserv() {
         <h1 className='text-5xl font-bold'>Webserv</h1>
         <div className='font-bold opacity-50'>42 Seoul July 2023</div>
       </div>
-      <div className='relative w-full rounded-3xl overflow-hidden flex justify-center items-center pt-8'>
+      <div className='relative w-full rounded-3xl overflow-hidden flex justify-center items-center p-8'>
         <div className='absolute inset-0 border-8 border-color2 border-opacity-50 rounded-3xl pointer-events-none'></div>
         <Image
           src='/image/webserv.png'
@@ -17,7 +17,7 @@ export default async function Webserv() {
           height={400}
           alt='webserv'
         />
-        <div className='absolute inset-x-0 bottom-0 h-60 bg-gradient-to-t from-background to-transparent' />
+        <div className='absolute inset-x-0 bottom-0 h-60 bg-gradient-to-t from-background from-10% to-transparent' />
       </div>
       <Overview />
       <Highlights />
