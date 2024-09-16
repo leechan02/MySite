60b718d3ce2a2439b43d32e920df9c16840d0a80
Eui Lee <lechan1317@gmail.com>
2024-09-16 20:14:52 +0900

    feat: mysite page

--- app/[locale]/(projects)/mysite/page.tsx
+++ app/[locale]/(projects)/mysite/page.tsx
@@ -1,7 +1,31 @@
-export default async function MySite() {
+import Overview from "../_components/Overview";
+import Highlights from "../_components/Highlights";
+import Content from "../_components/Content";
+import TableOfContents from "../_components/TableOfContents";
+import { unstable_setRequestLocale } from "next-intl/server";
+
+export default async function MySite({
+  params: { locale },
+}: {
+  params: { locale: string };
+}) {
+  unstable_setRequestLocale(locale);
+
   return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4">MySite Project</h1>
+    <div className='flex flex-col justify-center items-center gap-24 pb-24 font-mono text-foreground w-[320px] sm:w-[640px] lg:w-[900px] '>
+      <Content
+        link='https://github.com/leechan02/MySite'
+        image='/image/mysite.png'
+        project='mysite.content'
+        brColor='border-color3'
+      />
+      <section id='overview'>
+        <Overview project='mysite.overview' />
+      </section>
+      <section id='highlights'>
+        <Highlights project='mysite.highlights' bgColor='bg-color3' />
+      </section>
+      <TableOfContents sections={["Overview", "Highlights"]} />
     </div>
   );
 }
