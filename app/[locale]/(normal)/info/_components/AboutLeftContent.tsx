0cfb9ba45e45d9d8f5edc58fd9151a78b46afcad
Eui Lee <lechan1317@gmail.com>
2024-09-17 00:10:31 +0900

    feat: about translate

--- app/[locale]/(normal)/info/_components/AboutLeftContent.tsx
+++ app/[locale]/(normal)/info/_components/AboutLeftContent.tsx
@@ -1,28 +1,26 @@
+import { useTranslations } from "next-intl";
 import Image from "next/image";
 
 export default function AboutLeftContent() {
+  const t = useTranslations("info.about");
   return (
     <div className='flex flex-col justify-center items-center gap-16 w-[300px] lg:w-[400px]'>
       <div className='flex flex-col justify-center items-start gap-4'>
         <div className='text-sm font-mono text-foreground font-bold'>
-          My background in Computer Science
+          {t("leftFirstTitle")}
         </div>
-        <div className='text-xs font-mono text-foreground opacity-50'>
-          In 2022, I started my journey into development without knowing how to
-          code or use a terminal. As I grew more curious about the world, I
-          recognized many inefficiencies around me and realized the importance
-          of problem-solving.
-        </div>
-        <div className='text-xs font-mono text-foreground opacity-50'>
-          This led me to 42 Seoul, where I immersed myself in computer science
-          fundamentals such as operating systems, data structures, and networks.
-          Currently, I am focusing on frontend development to address real-world
-          challenges.
-        </div>
+        {t.raw("leftFirstContent").map((member: string, index: number) => (
+          <div
+            key={index}
+            className='text-xs font-mono text-foreground opacity-50'
+          >
+            {member}
+          </div>
+        ))}
       </div>
       <div className='relative w-[300px] lg:w-[400px] h-[400px] border-10 border-color1 rounded-[40px] overflow-hidden'>
         <Image
-          src='/image/profile.jpg'
+          src='/image/mountain.jpg'
           alt='profile'
           fill
           style={{ objectFit: "cover" }}
@@ -30,19 +28,16 @@ export default function AboutLeftContent() {
       </div>
       <div className='flex flex-col justify-center items-start gap-4'>
         <div className='text-sm font-mono text-foreground font-bold'>
-          Coding style
+          {t("leftSecondTitle")}
         </div>
-        <div className='text-xs font-mono text-foreground opacity-50'>
-          I appreciate beauty, which is why I prioritize writing clean,
-          understandable, and simple code. While optimization is important, I
-          don&apos;t believe complex code is always better-performing.
-        </div>
-        <div className='text-xs font-mono text-foreground opacity-50'>
-          As a developer, I value communication skills, especially since
-          development is rarely a solo effort. Starting from scratch has taught
-          me how to explain concepts clearly and in a way that others can easily
-          understand.
-        </div>
+        {t.raw("leftSecondContent").map((member: string, index: number) => (
+          <div
+            key={index}
+            className='text-xs font-mono text-foreground opacity-50'
+          >
+            {member}
+          </div>
+        ))}
       </div>
       <div className='relative w-[300px] lg:w-[400px] h-[400px] border-10 border-color1 rounded-[40px] overflow-hidden'>
         <Image
--- app/[locale]/(normal)/info/_components/AboutRightContent.tsx
+++ app/[locale]/(normal)/info/_components/AboutRightContent.tsx
@@ -1,11 +1,13 @@
+import { useTranslations } from "next-intl";
 import Image from "next/image";
 
 export default function AboutRightContent() {
+  const t = useTranslations("info.about");
   return (
     <div className='flex flex-col justify-center items-center gap-16 w-[300px] lg:w-[400px]'>
       <div className='relative w-[300px] lg:w-[400px] h-[400px] border-10 border-color1 rounded-[40px] overflow-hidden'>
         <Image
-          src='/image/mountain.jpeg'
+          src='/image/profile.jpeg'
           alt='profile'
           fill
           style={{ objectFit: "cover" }}
@@ -13,21 +15,16 @@ export default function AboutRightContent() {
       </div>
       <div className='flex flex-col justify-center items-start gap-4'>
         <div className='text-sm font-mono text-foreground font-bold'>
-          But I want more
+          {t("rightFirstTitle")}
         </div>
-        <div className='text-xs font-mono text-foreground opacity-50'>
-          I have a lot of curiosity and a strong desire to learn. The more I
-          study, the more I realize how much I don’t know. Since I&apos;ve had less
-          time to study than others, I’ve worked even harder to catch up.
-        </div>
-        <div className='text-xs font-mono text-foreground opacity-50'>
-          Through this, I&apos;ve learned that while things can be difficult, nothing
-          is impossible. I&apos;ve solved problems, completed over 300 code reviews,
-          and written over 10,000 lines of code in a single project. This has
-          made me unafraid of learning new technologies. Despite having no prior
-          experience with frameworks like React or Next.js, I built a website
-          using Next.js in just two weeks.
-        </div>
+        {t.raw("rightFirstContent").map((member: string, index: number) => (
+          <div
+            key={index}
+            className='text-xs font-mono text-foreground opacity-50'
+          >
+            {member}
+          </div>
+        ))}
       </div>
       <div className='relative w-[300px] lg:w-[400px] h-[400px] border-10 border-color1 rounded-[40px] overflow-hidden'>
         <Image
@@ -39,19 +36,16 @@ export default function AboutRightContent() {
       </div>
       <div className='flex flex-col justify-center items-start gap-4'>
         <div className='text-sm font-mono text-foreground font-bold'>
-          In my spare time,
+          {t("rightSecondTitle")}
         </div>
-        <div className='text-xs font-mono text-foreground opacity-50'>
-          I enjoy learning, taking on challenges, and documenting my journey.
-        </div>
-        <div className='text-xs font-mono text-foreground opacity-50'>
-          Out of curiosity, I read books to learn. I love challenges, so I dive
-          in and try things—recently, I climbed the Himalayas. I believe that
-          knowledge becomes truly mine when I write and speak about it, which is
-          why I run a YouTube channel like a personal journal. While I’m not a
-          huge fan of exercise, I do it because I need to stay healthy to keep
-          doing what I love.
-        </div>
+        {t.raw("rightSecondContent").map((member: string, index: number) => (
+          <div
+            key={index}
+            className='text-xs font-mono text-foreground opacity-50'
+          >
+            {member}
+          </div>
+        ))}
       </div>
     </div>
   );
