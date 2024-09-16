bd52f120ba73eedb0f5a87c20e221f2fcb87895c
Eui Lee <lechan1317@gmail.com>
2024-09-16 13:13:13 +0900

    feat: overview on webserv

---
+++ app/[locale]/(projects)/webserv/_components/Overview.tsx
@@ -0,0 +1,60 @@
+export default function Overview() {
+  return (
+    <div className='flex flex-col sm:flex-row justify-between items-start gap-8 sm:gap-16'>
+      <div className='flex flex-col justify-start items-start gap-8 w-full sm:w-1/2'>
+        <div className='flex flex-col gap-2 w-full'>
+          <div className='text-sm font-bold'>MyRole</div>
+          <div className='text-xs opacity-50'>
+            Architecture Design, HTTP Parsing
+          </div>
+        </div>
+        <div className='flex flex-col gap-2 w-full'>
+          <div className='text-sm font-bold'>Team</div>
+          <div className='text-xs opacity-50 flex flex-col gap-2'>
+            <div>Jinho Heo</div>
+            <div>Hyeonjun An</div>
+            <div>Yunseon Im</div>
+          </div>
+        </div>
+        <div className='flex flex-col gap-2 w-full'>
+          <div className='text-sm font-bold'>Timeline</div>
+          <div className='text-xs opacity-50'>4 Months, starts July 2023</div>
+        </div>
+        <div className='flex flex-col gap-2 w-full'>
+          <div className='text-sm font-bold'>Tech Keywords</div>
+          <div className='text-xs opacity-50'>
+            C++98, Makefile, HTML, CSS, OOP, Non-block I/O, I/O Multiplexing
+            (kqueue), HTTP/1.1
+          </div>
+        </div>
+      </div>
+      <div className='flex flex-col gap-4 justify-start items-start w-full sm:w-1/2'>
+        <div className='text-sm font-bold'>Overview</div>
+        <div className='text-xs opacity-50 flex flex-col gap-4'>
+          <div>
+            Webserv is an ambitious project undertaken as part of the School 42
+            curriculum, challenging students to develop a robust HTTP/1.1
+            compatible web server in C++ 98. This project pushes the boundaries
+            of low-level network programming, requiring the implementation of
+            non-blocking I/O operations and efficient request handling.
+          </div>
+          <div>
+            Key features include the use of I/O multiplexing with `poll()` (or
+            equivalent), ensuring the server never blocks or hangs indefinitely,
+            and the ability to serve static websites while supporting multiple
+            HTTP methods.
+          </div>
+          <div>
+            By implementing these advanced concepts, Webserv not only serves as
+            a practical learning tool for understanding web server architecture
+            but also demonstrates the power and flexibility of C++ in creating
+            high-performance network applications. The successful completion of
+            this project showcases a deep understanding of network protocols,
+            system-level programming, and efficient resource management in a
+            challenging, real-world scenario.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/[locale]/(projects)/webserv/page.tsx
+++ app/[locale]/(projects)/webserv/page.tsx
@@ -1,13 +1,15 @@
 import Image from "next/image";
+import Overview from "./_components/Overview";
 
 export default async function Webserv() {
   return (
-    <div className='flex flex-col justify-center items-center gap-10 font-mono text-foreground'>
+    <div className='flex flex-col justify-center items-center gap-10 font-mono text-foreground w-[320px] sm:w-[640px] lg:w-[900px] '>
       <div className="flex flex-col text-center gap-4">
         <h1 className="text-5xl font-bold">Webserv</h1>
         <div className="font-bold opacity-50">42 Seoul July 2023</div>
       </div>
       <Image src="/image/webserv.png" width={800} height={400} alt="webserv" />
+      <Overview />
       <div className='w-[320px] sm:w-[640px] lg:w-[900px] border border-foreground opacity-10' />
     </div>
   );
