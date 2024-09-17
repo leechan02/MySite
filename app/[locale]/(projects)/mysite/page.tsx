941bbaecab6c9a9e11b70b1be227b7cb4692222c
Eui Lee <lechan1317@gmail.com>
2024-09-18 00:35:36 +0900

    feat: mysite page image

--- app/[locale]/(projects)/mysite/page.tsx
+++ app/[locale]/(projects)/mysite/page.tsx
@@ -11,6 +11,29 @@ export default async function MySite({
 }) {
   unstable_setRequestLocale(locale);
 
+  const highlightMediaItems = [
+    {
+      type: "video" as const,
+      src: "/video/mysite.mp4",
+    },
+    {
+      type: "image" as const,
+      src: "/image/mysite/mysite.png",
+    },
+    {
+      type: "image" as const,
+      src: "/image/mysite/myinfo.png",
+    },
+    {
+      type: "image" as const,
+      src: "/image/mysite/myko.png",
+    },
+    {
+      type: "image" as const,
+      src: "/image/mysite/mysiteMobile.png",
+    }
+  ];
+
   return (
     <div className='flex flex-col justify-center items-center gap-24 pb-24 font-mono text-foreground w-[320px] sm:w-[640px] lg:w-[900px] '>
       <Content
@@ -23,7 +46,11 @@ export default async function MySite({
         <Overview project='mysite.overview' />
       </section>
       <section id='highlights'>
-        <Highlights project='mysite.highlights' bgColor='bg-color4' />
+        <Highlights
+          project='mysite.highlights'
+          bgColor='bg-color4'
+          mediaItems={highlightMediaItems}
+        />
       </section>
       <TableOfContents sections={["Overview", "Highlights"]} />
     </div>
