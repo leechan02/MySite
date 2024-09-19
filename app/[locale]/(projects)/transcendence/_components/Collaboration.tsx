c2e8d0532995bb1ea58bdf2b6d48a351a399a319
Eui Lee <lechan1317@gmail.com>
2024-09-19 14:36:33 +0900

    feat: collaboration section add

--- app/[locale]/(projects)/transcendence/_components/Collaboration.tsx
+++ app/[locale]/(projects)/transcendence/_components/Collaboration.tsx
@@ -1,7 +1,14 @@
 import Content from "@/components/content/Content";
+import ImageBox from "@/components/content/ImageBox";
 import Title from "@/components/content/Title";
+import { useTranslations } from "next-intl";
+import { Fragment } from "react";
+import { FiArrowRight } from "react-icons/fi";
 
 export default function Collaboration() {
+  const t = useTranslations("transcendence.collaboration");
+  const gitflowItems = t.raw("gitflow");
+
   return (
     <section
       id='collaboration'
@@ -13,11 +20,26 @@ export default function Collaboration() {
         title='first'
         content='firstContent'
       />
+      <div className='bg-foreground/5 shadow border-2 border-foreground/10 rounded-2xl sm:rounded-[40px] backdrop-blur-[15px] w-full overflow-hidden flex flex-wrap justify-center items-center p-8 gap-2'>
+        {gitflowItems.map((item: string, index: number) => (
+          <Fragment key={index}>
+            <div className='flex justify-center items-center gap-2 rounded-full border-2 border-foreground/5 backdrop-blur-md py-2 px-4'>
+              <div className='font-semibold font-mono text-foreground text-xs'>
+                {item}
+              </div>
+            </div>
+            {index < gitflowItems.length - 1 && (
+              <FiArrowRight className='text-foreground text-xs' />
+            )}
+          </Fragment>
+        ))}
+      </div>
       <Content
         project='transcendence.collaboration'
         title='second'
         content='secondContent'
       />
+      <ImageBox src='/image/pong/pongNotion.png' alt='Notion Collaboration' />
     </section>
   );
 }
