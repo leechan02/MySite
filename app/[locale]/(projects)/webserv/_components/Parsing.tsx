8da89a11f62ea6e55caa2d477fca792878c13b12
Eui Lee <lechan1317@gmail.com>
2024-09-20 17:06:37 +0900

    feat: parsing script

--- app/[locale]/(projects)/webserv/_components/Parsing.tsx
+++ app/[locale]/(projects)/webserv/_components/Parsing.tsx
@@ -10,7 +10,7 @@ interface CodeItem {
   label: string;
 }
 
-export default function Http() {
+export default function Parsing() {
   const codeItems: CodeItem[] = [
     {
       code: `class Http {
