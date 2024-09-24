a4e892720e349c6699a0ca584bcf6ce73ae8f85f
Eui Lee <lechan1317@gmail.com>
2024-09-24 15:07:32 +0900

    fix: button link position

--- components/button/Button.tsx
+++ components/button/Button.tsx
@@ -1,3 +1,6 @@
+"use client";
+import { motion } from 'framer-motion';
+
 interface ButtonProps {
   label: string;
   icon?: React.ElementType | string;
@@ -8,9 +11,12 @@ export default function Button({
   icon: Icon,
 }: ButtonProps) {
   return (
-    <div className="flex justify-center items-center gap-2 rounded-full border-2 border-foreground/5 backdrop-blur-md py-2 pl-4 pr-6">
+    <motion.div
+      className="flex justify-center items-center gap-2 rounded-full border-2 border-foreground/5 backdrop-blur-md py-2 pl-4 pr-6"
+      whileHover={{ y: -5, transition: { duration: 0.2 } }}
+    >
       {Icon && <Icon className="w-4 h-4 text-foreground" />}
       <div className="font-semibold font-mono text-foreground font-xs">{label}</div>
-    </div>
+    </motion.div>
   );
 }
--- components/header/ProjectHeader.tsx
+++ components/header/ProjectHeader.tsx
@@ -1,11 +1,14 @@
+"use client";
 import Link from "next/link";
 import Button from "../button/Button";
 import { FiArrowLeft } from "react-icons/fi";
 
 export default function ProjectHeader() {
   return (
-    <Link className='flex justify-start items-center p-8 sm:p-12' href='/'>
-      <Button label='Back' icon={FiArrowLeft} />
-    </Link>
+    <div className='flex justify-start items-center p-8 sm:p-12'>
+      <Link href='/'>
+        <Button label='Back' icon={FiArrowLeft} />
+      </Link>
+    </div>
   );
 }
